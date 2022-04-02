0b780ec377ba791a4275bd1d668065c119bb84c9
sly503 <fatjonrami@gmail.com>
2022-04-02 15:21:20 +0200

    added Api and Util and Hardcode test on product microservice with productserviceImp

--- microservices/product-service/src/main/java/com/frami101/productservice/ProductServiceApplication.java
+++ microservices/product-service/src/main/java/com/frami101/productservice/ProductServiceApplication.java
@@ -2,8 +2,10 @@ package com.frami101.productservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.frami101")
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
---
+++ microservices/product-service/src/main/java/com/frami101/productservice/services/ProductServiceImpl.java
@@ -0,0 +1,39 @@
+package com.frami101.productservice.services;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+import com.frami101.api.core.product.Product;
+import com.frami101.api.core.product.ProductService;
+import com.frami101.api.exceptions.InvalidInputException;
+import com.frami101.api.exceptions.NotFoundException;
+import com.frami101.util.http.ServiceUtil;
+
+@RestController
+public class ProductServiceImpl implements ProductService {
+
+  private static final Logger LOG = LoggerFactory.getLogger(ProductServiceImpl.class);
+
+  private final ServiceUtil serviceUtil;
+
+  @Autowired
+  public ProductServiceImpl(ServiceUtil serviceUtil) {
+    this.serviceUtil = serviceUtil;
+  }
+
+  @Override
+  public Product getProduct(int productId) {
+    LOG.debug("/product return the found product for productId={}", productId);
+
+    if (productId < 1) {
+      throw new InvalidInputException("Invalid productId: " + productId);
+    }
+
+    if (productId == 13) {
+      throw new NotFoundException("No product found for productId: " + productId);
+    }
+
+    return new Product(productId, "name-" + productId, 123, serviceUtil.getServiceAddress());
+  }
+}
