e54cbbbd2fa7eb519177da81936d15c6d4134174
sly503 <fatjonrami@gmail.com>
2022-04-02 22:46:28 +0200

    added composite microservice

--- microservices/recommendation-service/src/main/java/com/frami101/recommendationservice/RecommendationServiceApplication.java
+++ microservices/recommendation-service/src/main/java/com/frami101/recommendationservice/RecommendationServiceApplication.java
@@ -2,8 +2,10 @@ package com.frami101.recommendationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.frami101")
 public class RecommendationServiceApplication {
 
 	public static void main(String[] args) {
--- microservices/review-service/src/main/java/com/frami101/reviewservice/ReviewServiceApplication.java
+++ microservices/review-service/src/main/java/com/frami101/reviewservice/ReviewServiceApplication.java
@@ -2,8 +2,10 @@ package com.frami101.reviewservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.frami101")
 public class ReviewServiceApplication {
 
 	public static void main(String[] args) {
