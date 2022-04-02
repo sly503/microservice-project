ec116a451f393702cbb03da5a06b4773aaec2a1b
sly503 <fatjonrami@gmail.com>
2022-04-02 12:00:04 +0200

    initial commit , multiproject build setup

---
+++ microservices/recommendation-service/src/main/java/com/frami101/recommendationservice/RecommendationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.frami101.recommendationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecommendationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecommendationServiceApplication.class, args);
+	}
+
+}
